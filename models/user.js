b9e2830aa37932a600dcce82ab150242158f53aa
marysgreenwood <marysgreenwood@gmail.com>
2023-01-25 13:46:52 -0500

    added user schema

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Schema, Types } = require("mongoose");
+
+//schema for user model
+const userSchema = new Schema(
+  {
+    user_id: {
+      type: Schema.type.ObjectId(),
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: this,
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+//virtual to track friend count
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+//define & export model
+const User = model("user", userSchema);
+
+module.exports = User;
